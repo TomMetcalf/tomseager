320ef1b138fadf89d0b81d897944bcbedd14284f
Tom Metcalf <intermet@gmail.com>
2023-10-15 12:53:56 +0100

    add styling for reponsive design on smaller devices

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>www.tom-seager.com</h1>
-      <h2>The home of Tom Seager</h2>
+      <h1 className="web-address">www.tom-seager.com</h1>
+      <h2>
+        <span className="first-line">The Home of </span>
+        <span className="second-line">Tom Seager</span>
+      </h2>
       <p>Site currently under construction.</p>
       <p>Please check back for future updates.</p>
       <section className="siteLinks">
@@ -13,14 +16,14 @@ function App() {
           className="siteLinkStyle"
           target="_blank"
         >
-          Visit Tom Seager's Art Site
+          Visit Tom Seager Art Site
         </a>
         <a
           href="https://dev.tom-seager.com"
           className="siteLinkStyle"
           target="_blank"
         >
-          Visit Tom Seager's Dev Site
+          Visit Tom Seager Dev Site
         </a>
       </section>
     </>
