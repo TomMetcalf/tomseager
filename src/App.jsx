86525e95a5d6b929d9771e75c20bd7bb755bfafa
Tom Metcalf <intermet@gmail.com>
2023-10-15 11:33:59 +0100

    add basic links to subdomains

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>www.tom-seager.com</h1>
       <h2>The home of Tom Seager</h2>
       <p>Site currently under construction.</p>
       <p>Please check back for future updates.</p>
+      <section className="siteLinks">
+        <a
+          href="https://art.tom-seager.com"
+          className="siteLinkStyle"
+          target="_blank"
+        >
+          Visit Tom Seager's Art Site
+        </a>
+        <a
+          href="https://dev.tom-seager.com"
+          className="siteLinkStyle"
+          target="_blank"
+        >
+          Visit Tom Seager's Dev Site
+        </a>
+      </section>
     </>
   );
 }
